79d28a28f23e2ad4423184f2ff8489eea9ba8446
adriancom <adrian@gmail.com>
2019-03-22 01:20:30 -0300

    insere timeout

--- script_tj.sh
+++ script_tj.sh
@@ -4,11 +4,11 @@ rm *.html resultados.txt
 for nome in $(cat nomes.txt  | sed -e 's/ /+/g'); do
 for competencia in $(seq 12 && seq 16 18 && seq 21 23); do
 echo "Baixando" $nome "na 1a instância com o cod. de competência" $competencia
-curl 'http://www4.tjrj.jus.br/consultaProcessoNome/ConsultaNome.do' --data 'acao=consulta&descComarca=Todas+Comarcas&descCompetencia='$competencia'&novaTela=true&tipousuario=&idUsuSegWeb=&origem=1&tiposecinst=1&comarca=TODAS&competencia='$competencia'&anoInicio=1980&anoFinal=2018&dataInicio=&dataFim=21%2F03%2F2019&nomeParte='$nome'' > $nome'-instancia1-c'$competencia'.html'
+curl -m 60 'http://www4.tjrj.jus.br/consultaProcessoNome/ConsultaNome.do' --data 'acao=consulta&descComarca=Todas+Comarcas&descCompetencia='$competencia'&novaTela=true&tipousuario=&idUsuSegWeb=&origem=1&tiposecinst=1&comarca=TODAS&competencia='$competencia'&anoInicio=1980&anoFinal=2018&dataInicio=&dataFim=21%2F03%2F2019&nomeParte='$nome'' > $nome'-instancia1-c'$competencia'.html'
 done
 for origem in $(seq 9); do
 echo "Baixando" $nome "com a origem" $origem
-curl 'http://www4.tjrj.jus.br/consultaProcessoNome/ConsultaNome.do' --data 'acao=consulta&descComarca=Todas+Comarcas&novaTela=true&tipousuario=&idUsuSegWeb=&origem='$origem'&tiposecinst=1&comarca=TODAS&anoInicio=1980&anoFinal=2018&dataInicio=&dataFim=21%2F03%2F2020&nomeParte='$nome'' > $nome'-origem-c'$origem'.html'
+curl -m 60 'http://www4.tjrj.jus.br/consultaProcessoNome/ConsultaNome.do' --data 'acao=consulta&descComarca=Todas+Comarcas&novaTela=true&tipousuario=&idUsuSegWeb=&origem='$origem'&tiposecinst=1&comarca=TODAS&anoInicio=1980&anoFinal=2018&dataInicio=&dataFim=21%2F03%2F2020&nomeParte='$nome'' > $nome'-origem-c'$origem'.html'
 done
 done
 
